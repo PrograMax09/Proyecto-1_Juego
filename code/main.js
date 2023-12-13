e982e5253052c17a85d48ef425c808f312a84d5d
root <root@PrograMaxGS>
2023-12-13 19:55:50 +0000

    added WASD movement to player

--- code/main.js
+++ code/main.js
@@ -1,3 +1,37 @@
 //By: Max
 //Remember to import and export
-//If 'this.' doesn't work, put 'var file = this;'
+//If 'this.' doesn't work, put 'var self = this;'
+
+var self = this;
+
+var player = {
+    sprite : document.getElementById("player"),
+    x : 25,
+    y : 25,
+    isAlive : true
+}
+
+window.addEventListener('keydown', function(e) {
+        switch(e.key) {
+            case 'w':
+                player.y -= 30
+                player.sprite.style.top = player.y + 'px'
+                break
+            case 'a':
+                player.x -= 30
+                player.sprite.style.left = player.x + 'px'
+                break
+            case 's':
+                player.y += 30
+                player.sprite.style.top = player.y + 'px'
+                break
+            case 'd':
+                player.x += 30
+                player.sprite.style.left = player.x + 'px'
+                break
+    }
+})
+
+
+
+//if ()
