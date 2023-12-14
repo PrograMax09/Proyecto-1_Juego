9d2e54c908204769e89c8158244744b0b6d856c5
root <root@PrograMaxGS>
2023-12-14 20:42:38 +0000

    added player movement & collision

--- code/main.js
+++ code/main.js
@@ -2,36 +2,91 @@
 //Remember to import and export
 //If 'this.' doesn't work, put 'var self = this;'
 
+
+var board = document.querySelector('#gameScreen')
 var self = this;
 
-var player = {
-    sprite : document.getElementById("player"),
-    x : 25,
-    y : 25,
-    isAlive : true
-}
+function Player(x, y) {
+    this.sprite = document.getElementById("player")
+    this.x = x
+    this.y = y
+    this.direction = null
+    this.speed = 15
+    this.isAlive = true
 
-window.addEventListener('keydown', function(e) {
-        switch(e.key) {
+    this.sprite
+
+    this.insertPlayer = function() {
+        var newPlayer = document.createElement('div')
+        newPlayer.setAttribute('id', 'player')
+        newPlayer.style.top = this.y + 'px'
+        newPlayer.style.left = this.x + 'px'
+        board.appendChild(newPlayer)
+        this.sprite = newPlayer
+    }
+    this.move = function() {
+        switch(this.direction) {
             case 'w':
-                player.y -= 30
-                player.sprite.style.top = player.y + 'px'
+                this.y -= this.speed
                 break
             case 'a':
-                player.x -= 30
-                player.sprite.style.left = player.x + 'px'
+                this.x -= this.speed
                 break
             case 's':
-                player.y += 30
-                player.sprite.style.top = player.y + 'px'
+                this.y += this.speed
                 break
             case 'd':
-                player.x += 30
-                player.sprite.style.left = player.x + 'px'
+                this.x += this.speed
                 break
+        }
+        this.sprite.style.top = this.y + 'px'
+        this.sprite.style.left = this.x + 'px'
     }
-})
+}
+
+var player = new Player(100, 100)
+player.insertPlayer()
+
+window.addEventListener('keydown', function(e) {
+    if (
+        (e.key === 'w' && player.y >= 15) ||
+        (e.key === 'a' && player.x >= 15) ||
+        (e.key === 's' && player.y <= 735) ||
+        (e.key === 'd' && player.x <= 760)
+    ) {
+        player.direction = e.key;
+        player.move();
+    }
+    //player.move();
+});
+/*    switch(e.key) {
+        case 'w':
+            player.direction = "w"
+            break
+        case 'a':
+            player.direction = "a"
+            break
+        case 's':
+            player.direction = "s"
+            break
+        case 'd':
+            player.direction = "d"
+            break
+    }
+    */
+    
+
+
+
 
 
 
 //if ()
+
+
+
+
+
+
+
+
